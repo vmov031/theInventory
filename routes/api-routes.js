9bfceadbabbf1dc7a032625c70e58a10cd7dd962
dsimmon1 <simmons.diana93@gmail.com>
2017-11-30 22:32:23 -0800

    [basic folder outline with server.js]

---
+++ routes/api-routes.js
@@ -0,0 +1,37 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route 
+  app.get("/api/todos", function(req, res) {
+
+  });
+
+  // POST route 
+  app.post("/api/todos", function(req, res) {
+
+  });
+
+  // DELETE route 
+
+  app.delete("/api/todos/:id", function(req, res) {
+
+
+  });
+
+  // PUT route
+  app.put("/api/todos", function(req, res) {
+
+  });
+
+};
