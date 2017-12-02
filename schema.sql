da9c21d5eb043f8dbe138ef3d7619c8bb98c4199
Janna de guzman <jandeguzman@Jannas-MacBook-Pro.local>
2017-12-02 11:56:04 -0800

    log in updates

--- schema.sql
+++ schema.sql
@@ -1,4 +1,9 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS inventory;
--- Creates the "todolist" database --
-CREATE DATABASE inventory;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `first_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `mob_no` int(11) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
