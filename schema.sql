538578f462f65d8f9f5df9c06ef89180af0ced62
mateyo3 <mateoreyes720@gmail.com>
2017-12-04 21:31:47 -0800

    schema update database and create tables

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+-- connected to user login page --
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `first_name` text NOT NULL,
@@ -7,3 +8,30 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+
+-- Drops the tropical_inventory if it exists currently --
+DROP DATABASE IF EXISTS tropical_inventory;
+-- Creates the "tropical_inventory" database --
+CREATE DATABASE tropical_inventory;
+
+-- Creates the "inventory" table and column names --
+CREATE TABLE inventory(
+	id int NOT NULL AUTO_INCREMENT,
+	product_code VARCHAR(10),
+	description VARCHAR(500),
+	SF_Box VARCHAR(10),
+	dimension VARCHAR(500),
+	quantity INT(100),
+	total INT(100),
+	PRIMARY KEY (id)
+);
+
+-- Creates the "users" table and column names --
+CREATE TABLE users(
+	id int NOT NULL AUTO_INCREMENT,
+	username VARCHAR(50),
+	email VARCHAR(100),
+	password BINARY(60),
+	PRIMARY KEY (id)
+);
