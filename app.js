e0b291178b984f1c33802775ef513d0aa1b3cf86
Janna de guzman <jandeguzman@Jannas-MacBook-Pro.local>
2017-12-05 20:36:23 -0800

    some styling

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use("/css",express.static(__dirname + "/css"));
 app.use(session({
               secret: 'keyboard cat',
               resave: false,
