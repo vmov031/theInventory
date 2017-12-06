31e346d585b4bce461700e3a935991987282fd0f
Janna de guzman <jandeguzman@Jannas-MacBook-Pro.local>
2017-12-05 19:25:27 -0800

    updates to dashboard

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
               secret: 'keyboard cat',
               resave: false,
