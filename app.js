3f7501b9c78eeb64b06ce9247d30a4261be353ab
Janna de guzman <jandeguzman@Jannas-MacBook-Pro.local>
2017-12-09 13:51:05 -0800

    orders page and a bunch o stuff

--- app.js
+++ app.js
@@ -46,8 +46,9 @@ app.get('/login', routes.index);//call for login page
 app.post('/login', user.login);//call for login post
 app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
 app.get('/home/logout', user.logout);//call for logout
-app.get('/home/profile',user.profile);//to render users profile
+// app.get('/home/profile',user.profile);//to render users profile
 app.get('/home/stockcontrol',user.stockcontrol);
 app.get('/home/inventory',user.inventory);
+app.get('/home/orders',user.orders);
 //Middleware
 app.listen(8080)
