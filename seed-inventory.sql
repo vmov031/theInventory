ed972f0815e2fb53377633bf17fba10eb33b3dae
dsimmon1 <simmons.diana93@gmail.com>
2017-12-09 14:21:22 -0800

    [displays the information onto the api and changes in the seeds and server.js file]

--- seed-inventory.sql
+++ seed-inventory.sql
@@ -1,7 +1,7 @@
 USE tropical_inventory;
 
-INSERT INTO inventory (product_code, description, SF_Box, dimension, quantity, total)
-VALUES 	("wmbd",  "12MM White Mocha", "15.10", "12 x 165 x 1215mm", 113, 113),
-		("sm", "12MM Siberian Maple", "21.96", "12 x 125 x 1215mm", 728, 728), 
-		("SR", "Eng. Hs. Havea Sumatra Ruby", "9.85", "9/16 x 5 1/2", 250, 250), 
-		("SmB", "Eng.Sm.Birch", "19.38", "3/5 x 4.9 x RL", 72, 72);
+INSERT INTO inventories (product_code, description, SF_Box, dimension, quantity, total, location, createdAt, updatedAt)
+VALUES 	("wmbd",  "12MM White Mocha", "15.10", "12 x 165 x 1215mm", 113, 113, "north", NOW(), NOW()),
+		("sm", "12MM Siberian Maple", "21.96", "12 x 125 x 1215mm", 728, 728, "north", NOW(), NOW()), 
+		("SR", "Eng. Hs. Havea Sumatra Ruby", "9.85", "9/16 x 5 1/2", 250, 250, "north", NOW(), NOW()), 
+		("SmB", "Eng.Sm.Birch", "19.38", "3/5 x 4.9 x RL", 72, 72, "north", NOW(), NOW());
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
+const Sequelize = require('sequelize');
+
+const op = Sequelize.Op;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -19,6 +22,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static("public"));
 
 // Routes
+
 // =============================================================
 // require("./routes/index.js")(app);
 // require("./routes/user.js")(app);
@@ -26,10 +30,11 @@ app.use(express.static("public"));
 // require("./routes/api-routes.js")(app);
 // require("./routes/html-routes.js")(app);
 // require("./routes/user.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
