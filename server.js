e6d95e3c031f5bc310953caaab6e2fd15725041f
Vanessa M <vanessamontero@Vanessas-MacBook-Pro.local>
2017-12-10 10:36:44 -0800

    server comments

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+// var express = require("express");
+// var bodyParser = require("body-parser");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+// // Sets up the Express App
+// // =============================================================
+// var app = express();
+// var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
+// // Requiring our models for syncing
+// var db = require("./models");
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// // Sets up the Express app to handle data parsing
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Static directory
-app.use(express.static("public"));
+// // Static directory
+// app.use(express.static("public"));
 
-// Routes
-// =============================================================
-// require("./routes/index.js")(app);
-// require("./routes/user.js")(app);
-// require("./public/js/inventory-view-all.js")(app);
-// require("./routes/api-routes.js")(app);
-// require("./routes/html-routes.js")(app);
-// require("./routes/user.js")(app);
+// // Routes
+// // =============================================================
+// // require("./routes/index.js")(app);
+// // require("./routes/user.js")(app);
+// // require("./public/js/inventory-view-all.js")(app);
+// // require("./routes/api-routes.js")(app);
+// // require("./routes/html-routes.js")(app);
+// // require("./routes/user.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+// // Syncing our sequelize models and then starting our Express app
+// // =============================================================
+// db.sequelize.sync({ force: true }).then(function() {
+//   app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
