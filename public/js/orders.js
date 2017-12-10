9b90896b8279f5cda10e6d49084586a80dd85116
Sharareh Shafiei <sharareh@Shararehs-MacBook-Pro.local>
2017-12-10 12:23:27 -0800

    order.js function

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var productID = $(".product-id");
-  var quantity = $(".quantity");
-   var month = $(".month");
+  var quantityInput = $(".quantity");
+  var month = $(".month");
   var date = $(".date");
   var year = $(".year");
   // Our new orders will go inside the orderContainer
@@ -17,7 +17,7 @@ $(document).ready(function() {
   function handleReceiveOrder(event) {
     event.preventDefault();
     // Wont submit the order if we are missing product-id, quantity month , date and year
-    if (!productID.val().trim() || !quantity.val().trim() || !month.val()  || !date.val()  || !year.val()) {
+    if (!productID.val().trim() || !quantityInput.val().trim() || !month.val()  || !date.val()  || !year.val()) {
       return;
     }
     else {
@@ -28,7 +28,7 @@ $(document).ready(function() {
   function handleSendOrder(event) {
     event.preventDefault();
      // Wont submit the order if we are missing product-id, quantity month , date and year
-    if (!productID.val().trim() || !quantity.val().trim() || !month.val()  || !date.val()  || !year.val()) {
+    if (!productID.val().trim() || !quantityInput.val().trim() || !month.val()  || !date.val()  || !year.val()) {
       return;
     }
     else {
@@ -37,16 +37,18 @@ $(document).ready(function() {
   } 
 
   // This function increase the quantity in our database
-  function receiveOrder(quantity) {
+  function receiveOrder(quantityInput) {
     $.ajax({
       method: "PUT",
       url: "/orders",
       data: orderHistory
+    }).then(function() {
+     var newQuantity = quantity.val(data.quantity) - quantityInput;
     }).done(createNewRow);
   }
 
   // This function decrease the quantity in our database
-  function sendOrder(quantity) {
+  function sendOrder(quantityInput) {
     $.ajax({
       method: "PUT",
       url: "/orders",
@@ -56,19 +58,8 @@ $(document).ready(function() {
 
 // This function constructs an orderHistory-item row
   function createNewRow(orderHistory) {
-    var newRow = $(
-      [
-        "<li class='list-group-item order-item'>",
-        "<span>",
-        orderHistory.productID,
-        orderHistory.quantity,
-        orderHistory.month,
-        orderHistory.date,
-        orderHistory.year,
-        "</span>",
-        "</li>"
-      ].join("")
-    );
+  $(".order-container").append( "<li class='list-group-item order-item'>" + orderHistory.productID +" "+ orderHistory.quantityInput + " "+ orderHistory.month + " " + orderHistory.date + " " + orderHistory.year + "</li>")
+
   }
 
   });
