ecc94c39b02037e7f04f012a34f501429b4d9b1d
mateyo3 <mateoreyes720@gmail.com>
2017-12-11 22:12:12 -0800

    added instanceMethods and hooks to models/user.js

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+'use strict';
+var bcrypt = require("bcrypt-nodejs");
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     username: {
@@ -44,17 +47,20 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: [500]
       }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      }
+    },
+    // Hooks are automatic methods that run during various phases of the User Model lifecycle
+    // In this case, before a User is created, we will automatically hash their password
+    hooks: {
+      beforeCreate: function(user, options, cb) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+        cb(null, options);
+      }
     }
-  });
-
-  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-  };
-  // Hooks are automatic methods that run during various phases of the User Model lifecycle
-  // In this case, before a User is created, we will automatically hash their password
-  User.hook("beforeCreate", function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
 
   return User;
