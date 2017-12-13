dd97bd7c44505898ed5ce352abbbbbe35f7c16a6
Vanessa Montero <montero.vanessa@gmail.com>
2017-12-12 20:15:17 -0800

    Update orders.js

--- public/js/orders.js
+++ public/js/orders.js
@@ -43,11 +43,8 @@ $(document).ready(function() {
       url: "/orders",
       data: orderHistory
     }).then(function() {
-<<<<<<< HEAD
-     var newQuantity = quantity.val(data.quantity) + quantityInput;
-=======
-     var newQuantity = quantity.val(data.quantity) - quantityInput;
->>>>>>> 1301262c0a4b839a0b0a2f767668f2dcd2a83873
+
+     var newQuantityAdd = quantity.val(data.quantity) + quantityInput;
     }).done(createNewRow);
   }
 
@@ -64,12 +61,8 @@ $(document).ready(function() {
 
 // This function constructs an orderHistory-item row
   function createNewRow(orderHistory) {
-<<<<<<< HEAD
 
   $(".order-container").append( "<li class='list-group-item order-item'>" + orderHistory.productID +" "+ orderHistory.quantityInput + " "+ orderHistory.month + " " + orderHistory.date + " " + orderHistory.year + "</li>")
-=======
-    $(".order-container").append( "<li class='list-group-item order-item'>" + orderHistory.productID +" "+ orderHistory.quantityInput + " "+ orderHistory.month + " " + orderHistory.date + " " + orderHistory.year + "</li>")
->>>>>>> 1301262c0a4b839a0b0a2f767668f2dcd2a83873
 
   }
 
