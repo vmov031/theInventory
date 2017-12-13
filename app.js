4855808e11861ddc517c47a1f20a7799fcd46e97
mateyo3 <mateoreyes720@gmail.com>
2017-12-12 19:06:30 -0800

    added controller directory

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var bodyParser=require("body-parser");
 var connection = mysql.createConnection({
               host     : 'localhost',
               user     : 'root',
-              password : '',
+              password : 'Calamigos2015',
 
               database : 'tropical_inventory'
 
@@ -46,15 +46,15 @@ app.use(session({
 // development only
  
 app.get('/', routes.index);//call for main index page
-app.get('/signup', user.signup);//call for signup page
-app.post('/signup', user.signup);//call for signup post 
+// app.get('/signup', user.signup);//call for signup page
+// app.post('/signup', user.signup);//call for signup post 
 app.get('/login', routes.index);//call for login page
-app.post('/login', user.login);//call for login post
-app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
-app.get('/home/logout', user.logout);//call for logout
-// app.get('/home/profile',user.profile);//to render users profile
-app.get('/home/stockcontrol',user.stockcontrol);
-app.get('/home/inventory',user.inventory);
-app.get('/home/orders',user.orders);
+// app.post('/login', user.login);//call for login post
+// app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
+// app.get('/home/logout', user.logout);//call for logout
+// // app.get('/home/profile',user.profile);//to render users profile
+// app.get('/home/stockcontrol',user.stockcontrol);
+// app.get('/home/inventory',user.inventory);
+// app.get('/home/orders',user.orders);
 //Middleware
 app.listen(8080)
