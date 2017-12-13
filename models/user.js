fa994684909c7c291731361274ee274f946682e8
mateyo3 <mateoreyes720@gmail.com>
2017-12-12 18:12:36 -0800

    api-route fix. Found missing brackets

--- models/user.js
+++ models/user.js
@@ -33,21 +33,18 @@ module.exports = function(sequelize, DataTypes) {
         len: [12]
       }
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [12],
-        notEmpty: true
-      }
-    },
-    photo_url: {
+    photo: {
       type: DataTypes.TEXT,
       allowNull: false,
       validate: {
         len: [500]
       }
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      }
+    }, {
     instanceMethods: {
       validPassword: function(password) {
         return bcrypt.compareSync(password, this.password);
@@ -56,9 +53,8 @@ module.exports = function(sequelize, DataTypes) {
     // Hooks are automatic methods that run during various phases of the User Model lifecycle
     // In this case, before a User is created, we will automatically hash their password
     hooks: {
-      beforeCreate: function(user, options, cb) {
+      beforeCreate: function(user, options) {
         user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-        cb(null, options);
       }
     }
   });
