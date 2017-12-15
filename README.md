c85607a4f432a76419f9366b0b2d22bacd06638e
Janna de guzman <jandeguzman@Jannas-MacBook-Pro.local>
2017-12-15 01:54:17 -0800

    receive orders and read me

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# theInventory-
+Project Title:  Tropical Flooring Iventory Management System (TFIMS) - Simple, Beautiful, Powerful, 
+https://github.com/dsimmon1/theInventory.git
+
+TFIMS is a customized inventory management software for tracking inventory levels, orders, sales and deliveries for a hardwood flooring wholesale distributor. TFIMS was built with the specific needs of Tropical Flooring in mind. The major functionalities of TFIMS can be seen below: 
+	
+	Individual login (authenticated user) 
+	Dashboard - displays highlights and key metrics
+	Inventory - list of actual inventory database as well as a search bar
+	Orders - allows you to receive incoming and send out inventory orders
+	Stock control - Allows you to transfer stock from 1 location to the next (updates inventory stock levels)
+
+	TFIMS was built with the goal of improving the inventory and warehouse management process of Tropical Flooring. It would replace the manual inventory tracking with an intuitive, straight forward and simple system that all employees could learn
+
+
+Getting Started
+
+    Prerequisites: 
+        Internet connectivity - To load the application
+
+    Installing: 
+        Clicking Heroku  
+
+Deployment
+
+	Application is deployed on Heroku
+
+Built With 
+
+    HTML & CSS    
+    Bootstrap
+    Javascript
+    JQuery
+	Sequelize
+	Chart Library - 
+	Node JS
+	EJS
+
+Versioning
+
+	TFIMS 1.0
+
+Authors
+
+    de Guzman, Janna 
+    Montero, Vanessa
+    Reyes, Mateo
+    Shafiei, Sharareh 
+    Simmons, Dianna
+
+License
+
+Acknowledgements 
+    Paige - For always giving us her time and full attention every after 012928338 hours of TA-ing 
+    Omar - For giving us our foundation and believing that we could do more that we thought
--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 /**
-* Module dependencies.
-*/
-const express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , api = require('./routes/api-routes');
+ * Module dependencies.
+ */
+const express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    api = require('./routes/api-routes');
 //var methodOverride = require('method-override');
 const session = require('express-session'); //needed for Passport authentication
 // var passport = require("./config/passport"); //needed for Passport authentication
 const app = express();
 const db = require("./models");
 const Sequelize = require('sequelize');
-const mysql      = require('mysql');
-const bodyParser=require("body-parser");
+const mysql = require('mysql');
+const bodyParser = require("body-parser");
 const connection = mysql.createConnection({
-              host     : 'localhost',
-              user     : 'root',
-              password : '',
+    host: 'localhost',
+    user: 'root',
+    password: '',
 
-              database : 'tropical_inventory'
+    database: 'tropical_inventory'
 
-            });
- 
+});
+
 connection.connect();
 
 global.db = connection;
 var PORT = process.env.PORT || 8080;
 
- 
+
 // all environments
 // app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
@@ -37,65 +37,107 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use("/css",express.static(__dirname + "/css"));
-app.use("/js",express.static(__dirname + "/js"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
 app.use(session({
-              secret: 'keyboard cat',
-              resave: false,
-              saveUninitialized: true,
-              cookie: { maxAge: 120000000 }
-            }))
- 
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 120000000 }
+}))
+
 // development only
- 
-app.get('/', routes.index);//call for main index page
-app.get('/signup', user.signup);//call for signup page
-app.post('/signup', user.signup);//call for signup post 
-app.get('/login', routes.index);//call for login page
-app.post('/login', user.login);//call for login post
-app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
-app.get('/home/logout', user.logout);//call for logout
+
+app.get('/', routes.index); //call for main index page
+app.get('/signup', user.signup); //call for signup page
+app.post('/signup', user.signup); //call for signup post 
+app.get('/login', routes.index); //call for login page
+app.post('/login', user.login); //call for login post
+app.get('/home/dashboard', user.dashboard); //call for dashboard page after login
+app.get('/home/logout', user.logout); //call for logout
 // app.get('/home/profile',user.profile);//to render users profile
-app.get('/home/stockcontrol',user.stockcontrol);
-app.get('/home/inventory',user.inventory);
-app.get('/home/orders',user.orders);
+app.get('/home/stockcontrol', user.stockcontrol);
+app.get('/home/inventory', user.inventory);
+app.get('/home/orders', user.orders);
 // app.get('/home/api/inventory', api.api_inventory);
+ 
 
-app.put('/api/stockcontrol',  stockControl )
+ // HI SHAR, THIS NOW UPDATES THE COLUMN TOTAL when you click 
+app.put('/api/inventoryorders', Orders)
 
-function stockControl(res, req){
-  console.log('res SC', res.body)
-  const name = res.body.name
-  const locationFrom = res.body.locationFrom
-  const locationTo = res.body.locationTo
-  const amount = parseInt(res.body.amount)
+function Orders(res, req) {
 
-  db.Inventory
-    .findOne({
-        where: {
-            product_code: name
-        },
-    })
-    .then(function(data){
-      // console.log(data)
-      const product = data.dataValues
-      console.log(typeof product[locationFrom],typeof product[locationTo], typeof amount)
+    const ordersid = res.body.ordersid
+    const ordersquantity = parseInt(res.body.ordersquantity)
+    const ordersmonth = res.body.ordersmonth
+    const ordersday = res.body.ordersday
+    const ordersyear = res.body.ordersyear
 
-      db.Inventory
-        .update(
-          { 
-            [locationFrom]: product[locationFrom] - amount,  
-            [locationTo]:  product[locationTo] + amount 
-          },
-          { where: { product_code: name } }
-        )
-        .then(function(update){
-          console.log('update', update)
+    db.Inventory
+        .findOne({
+            where: {
+                product_code: ordersid
+            },
         })
-        .catch(function(err){
-          console.log('error', err)
+        .then(function(data) {
+            // console.log(data)
+            const product = data.dataValues
+            // console.log(typeof product[locationFrom],typeof product[locationTo], typeof amount)
+
+            db.Inventory
+                .update({
+
+                        total: ordersquantity + total
+                    }, // [locationFrom]: product[locationFrom] - amount,  
+
+                    { where: { product_code: ordersid } }
+                )
+                .then(function(update) {
+                    console.log('update', update)
+                })
+                .catch(function(err) {
+                    console.log('error', err)
+                })
         })
-    })
+}
+
+// this is the end of inventoryorders
+
+
+
+//start of stock control 
+app.put('/api/stockcontrol', stockControl)
+
+function stockControl(res, req) {
+    console.log('res SC', res.body)
+    const name = res.body.name
+    const locationFrom = res.body.locationFrom
+    const locationTo = res.body.locationTo
+    const amount = parseInt(res.body.amount)
+
+    db.Inventory
+        .findOne({
+            where: {
+                product_code: name
+            },
+        })
+        .then(function(data) {
+            // console.log(data)
+            const product = data.dataValues
+            console.log(typeof product[locationFrom], typeof product[locationTo], typeof amount)
+
+            db.Inventory
+                .update({
+                    [locationFrom]: product[locationFrom] - amount,
+                    [locationTo]: product[locationTo] + amount
+                }, { where: { product_code: name } })
+                .then(function(update) {
+                    console.log('update', update)
+                })
+                .catch(function(err) {
+                    console.log('error', err)
+                })
+        })
 }
 
 app.use(bodyParser.text());
@@ -105,9 +147,9 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 
 db.sequelize.sync({}).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });
 //Middleware
 // app.listen(8080)
